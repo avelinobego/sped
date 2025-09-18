4b41fb8ec987852f4ad0d54cd252ad6abda130fe
Avelino Bego <avelino.bego@gmail.com>
2025-09-18 00:16:20 -0300

    Remove unused eSocial package files and related test cases

--- esocial/envio/envio_lotes.go
+++
@@ -1,8 +0,0 @@
-package envio
-
-import "avelinobego/esocial/tipos"
-
-type EnvioLoteEventos struct {
-	IdEmpregador  tipos.IdentificadorPorDocumento `xml:"ideEmpregador"`
-	IdTransmissor tipos.IdentificadorPorDocumento `xml:"ideTransmissor"`
-}
