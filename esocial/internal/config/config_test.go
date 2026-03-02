6f384cc1f5d816e4c901844e086985565d6a01e3
Avelino Bego <avelino.bego@gmail.com>
2026-03-02 14:39:14 -0300

    chore: 🧹 novas dependências
    test: 🚦 novos testes

--- esocial/internal/config/config_test.go
+++ esocial/internal/config/config_test.go
@@ -6,6 +6,11 @@ import (
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	slog.Info("Get environment variable from os env")
+	m.Run()
+}
+
 func TestPropertiesonfig(t *testing.T) {
 	slog.Info("Environment", "DATABASE_URL", os.Getenv("DATABASE_URL"))
 }
--- esocial/pkg/util/especification.go
+++ esocial/pkg/util/especification.go
@@ -25,21 +25,3 @@ func Or[T any](specs ...Specification[T]) Specification[T] {
 		return false
 	}
 }
-
-func Not[T any](spec Specification[T]) Specification[T] {
-	return func(candidate T) bool {
-		return !spec(candidate)
-	}
-}
-
-func False[T any]() Specification[T] {
-	return func(candidate T) bool {
-		return false
-	}
-}
-
-func True[T any]() Specification[T] {
-	return func(candidate T) bool {
-		return true
-	}
-}
