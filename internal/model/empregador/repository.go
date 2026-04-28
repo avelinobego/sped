5806c472770461005b86bc82eb764eafb91ea42f
Avelino Bego <avelino.bego@gmail.com>
2026-04-27 23:06:53 -0300

    refactor: 🔨 mudança conceitos

--- internal/model/empregador/repository.go
+++ internal/model/empregador/repository.go
@@ -1,67 +1,28 @@
 package empregador
 
 import (
-	"fmt"
 	"sped/esocial/pkg/utils"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-type reposiroyPg struct {
-	db *sqlx.DB
-	tx *sqlx.Tx
+func GetEmpresas(q *sqlx.DB) (result []Empresas, err error) {
+	err = q.Select(&result, "SELECT * FROM empresas")
+	return
 }
 
-func NewRepositoryPg() *reposiroyPg {
-	return &reposiroyPg{}
-}
-
-func (c *reposiroyPg) Connect() error {
-	var err error
-	//TODO: tornar isso genérico
-	c.db, err = sqlx.Connect("postgres", "user=postgres password=postgres dbname=esocial sslmode=disable")
-	return err
-}
-
-func (c *reposiroyPg) BeginTx() error {
-	var err error
-	c.tx, err = c.db.Beginx()
-	return err
-}
-
-func (c *reposiroyPg) CommitTx() error {
-	if c.tx == nil {
-		return fmt.Errorf("transaction not open")
-	}
-	return c.tx.Commit()
-}
-
-func (c *reposiroyPg) RollbackTx() error {
-	if c.tx == nil {
-		return fmt.Errorf("transaction not open")
-	}
-	return c.tx.Rollback()
-}
-
-func (c *reposiroyPg) Close() error {
-	return c.db.Close()
-}
-
-func (c *reposiroyPg) GetEmpresas() ([]Empresas, error) {
-	var empresas []Empresas
-	err := c.db.Select(&empresas, "SELECT * FROM empresas")
-	if err != nil {
-		return nil, err
-	}
-	return empresas, nil
-}
-
-func (c *reposiroyPg) Insert(empresa *Empresas) error {
+func InsertEmpresa(tx *sqlx.Tx, empresa *Empresas) error {
 	query, err := utils.BuildInsert("empresas", empresa)
 	if err != nil {
 		return err
 	}
-	_, err = c.db.NamedExec(query, empresa)
+
+	_, err = tx.NamedExec(query, empresa)
 	return err
+}
+
+func InsertEmpresas(tx *sqlx.Tx, empresas []Empresas) error {
+	//TODO: Implementar inserção em lote
+	return nil
 }
