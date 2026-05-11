93ba8a3d275d85108c4fbd87a890ed81cfc0c37f
Avelino Bego <avelino.bego@gmail.com>
2026-05-10 23:29:05 -0300

    refactor: 🔨 struct vazia para referência

--- internal/model/empregador/repository.go
+++ internal/model/empregador/repository.go
@@ -1,28 +1,35 @@
 package empregador
 
 import (
+	"context"
 	"sped/esocial/pkg/utils"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-func GetEmpresas(q *sqlx.DB) (result []Empresas, err error) {
-	err = q.Select(&result, "SELECT * FROM empresas")
+var vazio = Empresas{}
+
+func GetEmpresas(context context.Context, q *sqlx.DB) (result []Empresas, err error) {
+	err = q.SelectContext(context, &result, "SELECT * FROM empresas")
 	return
 }
 
-func InsertEmpresa(tx *sqlx.Tx, empresa *Empresas) error {
-	query, err := utils.BuildInsert("empresas", empresa)
+func InsertEmpresa(context context.Context, tx *sqlx.Tx, empresa *Empresas) error {
+	query, err := utils.BuildInsert("empresas", vazio)
 	if err != nil {
 		return err
 	}
 
-	_, err = tx.NamedExec(query, empresa)
+	_, err = tx.NamedExecContext(context, query, empresa)
 	return err
 }
 
-func InsertEmpresas(tx *sqlx.Tx, empresas []Empresas) error {
-	//TODO: Implementar inserção em lote
-	return nil
+func InsertEmpresas(context context.Context, tx *sqlx.Tx, empresas []Empresas) error {
+	query, err := utils.BuildInsert("empresas", vazio)
+	if err != nil {
+		return err
+	}
+	_, err = tx.NamedExecContext(context, query, empresas)
+	return err
 }
